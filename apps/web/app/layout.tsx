d69e6c633098488a9ffa80becf6101e577501312
Daniel Cherutti <DanielCherutti@users.noreply.github.com>
2026-02-02 14:21:06 -0300

    Add application icon to metadata in layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Pazini Monitoramento",
+  icons: { icon: "/images/icon.png" },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
